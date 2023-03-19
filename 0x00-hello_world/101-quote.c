f19a3c1e808ba8e99fcfd6d4ec52343de7d33e61
paul-mulatya <mulatyapaul44@gmail.com>
2023-03-19 22:27:37 +0000

    corr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main-print and that piece of art is useful" - Dora Korpar, 2015-10-19
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	write(2,"and that piece of  art is useful \" - Dora Korpar, 2015-10-19\n",61);
+	write(2, "and that piece of  art is useful \" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
 
