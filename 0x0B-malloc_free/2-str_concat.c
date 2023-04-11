7e973894a935c20b3602486955e8ba6f0d1905e7
paul-mulatya <mulatyapaul44@gmail.com>
2023-04-11 21:16:43 +0000

    corrected betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,12 +6,11 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-		
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
-		
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
