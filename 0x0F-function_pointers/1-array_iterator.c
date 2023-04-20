e042634cf0b8a04a3a4073c19bba2b827e08fd00
paul-mulatya <mulatyapaul44@gmail.com>
2023-04-20 19:31:26 +0000

    added function_pointers.h and checked betty documentation

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * array_iterator - prints each array 
+ * array_iterator- prints each array
  * @array: array
  * @size: how many elem to print
  * @action: pointer to print
